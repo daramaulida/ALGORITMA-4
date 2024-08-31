be656b17d9c474967fae707efe8d176aee4f9b9e
daramaulida <daraksp291@gmail.com>
2024-08-31 11:20:07 +0700

    perulangan for

---
+++ 01 perulangan for.php
@@ -0,0 +1,11 @@
+<?php
+
+for($i = 0; $i <= 10; $i++){
+    echo "<h2>Ini perulangan ke-$i</h2>";
+}
+
+for($i = 5; $i < 10; $i++){
+    echo "<hr><h2>Ini perulangan ke-$i</h2>";
+}
+echo "<p><strong>By:Dara Maulida";
+?>
