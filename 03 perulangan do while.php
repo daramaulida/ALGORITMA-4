731b4a6917c0e3d0f247db330fbfb1254afb27ac
daramaulida <daraksp291@gmail.com>
2024-08-31 11:36:34 +0700

    perulangan do while

---
+++ 03 perulangan do while.php
@@ -0,0 +1,17 @@
+<?php 
+
+$ulangi = 5;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 0);
+
+$ulangi = 7;
+
+do {
+    echo "<p><hr>ini adalah perulangan ke-$ulangi</p><hr>";
+    $ulangi--;
+} while ($ulangi > 0);
+echo "<p><strong>By:Dara Maulida";
+?>
