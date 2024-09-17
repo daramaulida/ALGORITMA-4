1613a0151a962a8052d77b608deb1c06d4d39ac1
daramaulida <daraksp291@gmail.com>
2024-09-17 11:13:05 +0700

    array multidimensi

---
+++ 29 array multidimensi.php
@@ -0,0 +1,37 @@
+<?php
+// ini adalah array dua dimensi
+$matrik = [
+    [2,3,4],
+    [7,5,0],
+    [4,3,8],
+];
+
+// cara mengakses isinya
+echo $matrik[1][0]; //-> output: 7
+echo "<hr>";
+
+//contoh lain
+// membuat array 2 dimensi yang berisi array asosiatif
+
+$artikel = [
+    [
+        "judul" => "Belajar PHP & MySQL untuk Pemula",
+        "penulis" => "ahmadimuslim"
+    ],
+    [
+        "judul" => "Tutorial PHP dari Nol hingga Mahir",
+        "penulis" => "ahmadimuslim"
+    ],
+    [
+        "judul" => "Membuat Aplikasi Web dengan PHP",
+        "penulis" => "ahmadimuslim"
+    ]
+];
+
+// menampilkan array
+foreach($artikel as $post){
+    echo "<h2>".$post["judul"]."</h2>";
+    echo "<p>".$post["penulis"]."<p>";
+    echo "<hr>";
+}
+echo "<p><strong>By:Dara Maulida";
