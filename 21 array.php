1c79216a02fe75fe206b4327b81c0f6e48ea665c
daramaulida <daraksp291@gmail.com>
2024-09-17 10:32:53 +0700

    array

---
+++ 21 array.php
@@ -0,0 +1,5 @@
+<?php
+$nama_variabel=array("<br><hr>isi variabel1","<br><hr>isi variabel2","<br><hr>isi variabel3");
+echo "Variabel " . $nama_variabel[0] .",". $nama_variabel[1] .",". $nama_variabel[2] ."!<hr>";
+echo "<p><strong>By:Dara Maulida";
+?>
